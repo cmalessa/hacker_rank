763290e68836ff03954545d3f4b1a93de329f494
Casey Malessa <mail@cmalessa.com>
2017-03-05 21:21:00 -0800

    Add files via upload

--- cpp/algo/implementation/extra_long_fact.cpp
+++ cpp/algo/implementation/extra_long_fact.cpp
@@ -1,42 +1,85 @@
 //	Extra Long Factorials
 //	Author: vatsalchanana
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include <array>
 
 using namespace std;
 
 //	The number of digits in 100! is 158. We'll use a slightly greater length
 #define LENGTH 160
 
+void add(array<int, LENGTH>& a, array<int, LENGTH>& s);
+void mul(array<int, LENGTH>& a, int n);
+void fac(array<int, LENGTH>& a, int n);
+
 int main(void)
 {
-	int n, m, e;
+	int n;
 	cin >> n;
 
-	int f[LENGTH] = {1};
-	fill_n(f + 1, LENGTH, 0);
+	array<int, LENGTH> r;
+	r.fill(0);
+	r[0] = 1;
 
+	fac(r, n);
 
-	for (int i = 1; i < n; i++)
+	//	Print and ignore leading zeros
+	bool pad = true;
+	for (int i = LENGTH - 1; i >= 0; i--)
 	{
-	
-		for (int d = 0; d < LENGTH; d++)
+		if (pad && r[i] != 0)
 		{
-			m = f[d] * i;
-			e = 0;
-			while (m > 0)
-			{
-				f[d] = m % 10;
+			pad = false;
+		}
 
-			}
+		if (!pad)
+		{
+			cout << r[i];
 		}
 	}
 
 
-	
-
-
 	return 0;
+}
+
+//	These are hacked together so they're only useful for this particular problem.
+void add(array<int, LENGTH>& a, array<int, LENGTH>& s)
+{
+	int carry, add;
+
+	carry = 0;
+	for (int i = 0; i < LENGTH; i++)
+	{
+		add = carry + a[i] + s[i];
+
+		if (add > 9)
+		{
+			carry = 1;
+			a[i] = add - 10;
+		}
+		else
+		{
+			a[i] = add;
+			carry = 0;
+		}
+	}
+}
+
+void mul(array<int, LENGTH>& a, int n)
+{
+	array<int, LENGTH> temp;
+	temp = a;
+	for (int i = 1; i < n; i++)
+	{
+		add(a, temp);
+	}
+
+}
+
+void fac(array<int, LENGTH>& a, int n)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		mul(a, i);
+	}
 }
