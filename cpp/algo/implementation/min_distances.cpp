d794196c08b637a42cc5059567da15fbcce684e4
caseym <mail@cmalessa.com>
2017-03-16 22:50:09 -0700

    min_distances

--- cpp/algo/implementation/min_distances.cpp
+++ cpp/algo/implementation/min_distances.cpp
@@ -1,3 +1,35 @@
 //	Minimum Distances
 //	Author: Shafaet
+#include <cmath>
 #include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, min;
+	cin >> n;
+
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	min = n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[i] == a[j] && abs(i - j) < min)
+			{
+				min = abs(i - j);
+			}
+		} 
+	}
+
+	cout << (min == n ? -1 : min);
+
+
+	return 0;
+}
