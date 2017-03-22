ec04aedc60ffb7a6f4fc60d9a431fa0185910d3f
caseym <mail@cmalessa.com>
2017-03-21 20:17:16 -0700

    strings

--- cpp/algo/strings/two_chars.cpp
+++ cpp/algo/strings/two_chars.cpp
@@ -1,5 +1,7 @@
 //	Two Characters
 //	Author: nabila_ahmed
+#include <algorithm>
+#include <bitset>
 #include <iostream>
 #include <string>
 
@@ -7,6 +9,48 @@ using namespace std;
 
 int main(void)
 {
+	int len, max, cur, lastc;
+	cin >> len;
+
+	string s;
+	cin >> s;
+
+	bitset<26> in_s(0);
+	for_each(s.begin(), s.end(), [&in_s](char c){in_s[c - 'a'] = true;});
+
+	max = 0;
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		if (!in_s[i - 'a']) continue;
+
+		for (int j = 'a'; j <= 'z'; j++)
+		{
+			if(!in_s[j - 'a'] || i == j) continue;
+
+			lastc = -1;
+			cur = 0;
+			for (int k = 0; k < s.length(); k++)
+			{
+				if (s[k] == i || s[k] == j)
+				{
+					cur++;
+					if (s[k] != lastc)
+					{
+						lastc = s[k];
+					}
+					else
+					{
+						cur = 0;
+						break;
+					}
+				}
+			}
+
+			max = (cur > max) ? cur : max;
+		}
+	}
+
+	cout << max;
 
 
 	return 0;
